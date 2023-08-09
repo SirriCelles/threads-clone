440f9a938b17a5aa20f8907747558bc124e95add
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-08-09 22:10:48 +0100

    add authentication (sign-in and sign-up) using Clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+import '../globals.css';
+import type { Metadata } from "next";
+
+// Setting up SEO with NextJS
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'A Next.js 13 Meta Threads Application Clone'
+}
+
+const inter = Inter({ subsets: ['latin']});
+
+export default function RootLayout(
+  { children } : { children: React.ReactNode })
+  {
+    return (
+      // Inject the ClerkProvider to the root of auth to use features provided by Clerk
+      <ClerkProvider>
+        <html lang="en">
+          <body className={`${inter.className} bg-dark-1`}>
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
+    )
+  }
