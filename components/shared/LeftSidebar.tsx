9ce418437a9b559cf1a8e2573033b9f16752cf90
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-08-10 00:09:04 +0100

    added bottom sidebar bar for mobile navigation

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -21,7 +21,7 @@ const LeftSidebar = () => {
                 className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
               >
                 <Image src={link.imgURL} alt={link.label} width={24} height={24}/>
-  
+
                 <p className="text-light-1 max-lg:hidden">{link.label}</p>
               </Link>
             )
