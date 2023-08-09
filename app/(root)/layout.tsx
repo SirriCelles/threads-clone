aed21ea90a9375dc2e6e560fb3804c3d72c02472
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-08-09 22:53:51 +0100

    moved assets to public, add code for top bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,10 +3,12 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 
+import Topbar from '@/components/shared/Topbar';
 import Bottombar from '@/components/shared/Bottombar';
 import LeftSidebar from '@/components/shared/LeftSidebar';
 import RightSidebar from '@/components/shared/RightSidebar';
-import Topbar from '@/components/shared/TopBar';
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
