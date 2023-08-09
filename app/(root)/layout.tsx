ae643ac2540fc0c6be9756221556f8e7ef8b0726
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-08-09 22:17:21 +0100

    adding general layout of the app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+
+import Bottombar from '@/components/shared/Bottombar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Topbar from '@/components/shared/TopBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,7 @@
+const Topbar = () => {
+  return (
+    <h1>Topbar</h1>
+  )
+}
+
+export default Topbar
